a6b151fe53b654c760b1a71ed7f44d981b6d512a
Arkady Kidanov <a.kidanov@leadgid.ru>
2021-10-01 14:17:45 +0300

    save

---
+++ src/components/timeline.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="timeline">
+    <div class="timeline__text">asd</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+}
+</script>
+
+<style scoped lang="scss">
+.timeline {
+
+  &__text {
+    font-size: 34px;
+  }
+}
+</style>
